83f0de81e9280f78f149a6c45a3a64c74750441c
TK <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-07-09 15:52:20 +0200

    Commit in batches

--- packages/cms/src/lokalize/migrate-prd-to-dev.ts
+++ packages/cms/src/lokalize/migrate-prd-to-dev.ts
@@ -1,4 +1,5 @@
 import { assert } from '@corona-dashboard/common';
+import { chunk, set } from 'lodash';
 import { getClient } from '../client';
 import { LokalizeText } from './types';
 
@@ -9,6 +10,9 @@ import { LokalizeText } from './types';
  * This is done because we want to have matching document ids in both datasets
  * in order to implement logic around "move" mutations, and currently both
  * datasets use independent document ids for each lokalize key.
+ *
+ * Create a backup of lokalize documents first:
+ * `curl https://5mog5ask.api.sanity.io/v2021-06-07/data/export/development?types=lokalizeText > backup.ndjson`
  */
 (async function run() {
   const devClient = await getClient('development');
@@ -16,6 +20,10 @@ import { LokalizeText } from './types';
 
   const allDevDraftTexts =
     (await devClient.fetch(`*[_type == 'lokalizeText' && (_id in path("drafts.**"))] |
+   order(subject asc)`)) as LokalizeText[];
+
+  const allDevPublishedTexts =
+    (await devClient.fetch(`*[_type == 'lokalizeText' && !(_id in path("drafts.**"))] |
    order(subject asc)`)) as LokalizeText[];
 
   const allPrdPublishedTexts =
@@ -31,29 +39,33 @@ import { LokalizeText } from './types';
     await devClient.delete(doc._id);
   }
 
+  const devDocumentIdByKey = allDevPublishedTexts.reduce<
+    Record<string, string>
+  >((acc, v) => set(acc, v.key, v._id), {});
+
   /**
    * For each production document, look up the equivalent key in development and
    * delete that document to replace it with one from production stored under
    * the production document id.
    */
-  for (const doc of allPrdPublishedTexts) {
-    try {
-      const documentToDelete = await devClient.fetch(
-        `*[_type == 'lokalizeText' && key == '${doc.key}'][0]`
-      );
+  const chunks = chunk(allDevDraftTexts, 500);
 
-      assert(documentToDelete._id, 'No document _id in result');
+  for (const docs of chunks) {
+    const transaction = devClient.transaction();
 
-      console.log(`Replacing ${doc.key} from dev as ${documentToDelete._id}`);
+    for (const doc of docs) {
+      try {
+        const documentIdToDelete = devDocumentIdByKey[doc.key];
 
-      await devClient
-        .transaction()
-        .delete(documentToDelete._id)
-        .createOrReplace(doc)
-        .commit();
-    } catch (err) {
-      console.error(`Failed to process document ${doc.key}: ${err.message}`);
+        assert(documentIdToDelete, `No document id matched for key ${doc.key}`);
+
+        transaction.delete(documentIdToDelete).createOrReplace(doc);
+      } catch (err) {
+        console.error(`Failed to process document ${doc.key}: ${err.message}`);
+      }
     }
+    console.log(`Committing transaction...`);
+    await transaction.commit();
   }
 })().catch((err) => {
   console.error('An error occurred:', err.message);
